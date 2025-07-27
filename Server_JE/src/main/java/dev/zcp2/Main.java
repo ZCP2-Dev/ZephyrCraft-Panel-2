52d42b1d4dbb811cb4f39f3fbd487514b240780c
oneHerobrine <hero_brine@qq.com>
2025-07-27 22:28:43 +0800

    等3R60给我前端接口

--- Server_JE/src/main/java/dev/zcp2/Main.java
+++ Server_JE/src/main/java/dev/zcp2/Main.java
@@ -1,6 +1,8 @@
 package dev.zcp2;
 
 public final class Main {
+    private static final JsonConfig config = new JsonConfig("config", true);
     public static void main(String[] args) {
+        config.load();
     }
 }
