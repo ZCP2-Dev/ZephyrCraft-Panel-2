92f40fa462fcbbb58f5eb461783abae7bfea887a
Litezero <114818763+Litezero@users.noreply.github.com>
2025-07-28 12:19:14 +0800

    Delete Server_JE directory
    迁移至Server_Java仓库

--- Server_JE/src/main/java/dev/zcp2/Main.java
+++
@@ -1,8 +0,0 @@
-package dev.zcp2;
-
-public final class Main {
-    private static final JsonConfig config = new JsonConfig("config", true);
-    public static void main(String[] args) {
-        config.load();
-    }
-}
